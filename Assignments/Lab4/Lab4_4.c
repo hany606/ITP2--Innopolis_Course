a54db9520dcd3a2c651de9a4c7ae881eac2c22e8
Hany Hamed Aly Elanwar <h.hamed.elanwar@gmail.com>
2019-02-20 02:08:06 +0300

    Lab 4

--- Assignments/Lab4/Lab4_4.c
+++ Assignments/Lab4/Lab4_4.c
@@ -11,74 +11,75 @@ int main(int argc, char *argv[]) {
     int flagn = 0, flagx = 0, flagPattern = 0, flagFiles = 0;
     char *pattern;    
     for(int i = 1; i < argc; i++) {
-                            printf("%d%dfile: %s\n",flagn,flagx, argv[i]);
-
+        // printf("%d%dfile: %s\n",flagn,flagx, argv[i]);
         flagn = fmax(flagn,(argv[i][0] == '-' && (argv[i][1] == 'n' || (argv[i][2] != '\0' && argv[i][2] == 'n')) ? 1 : 0));
         flagx = fmax(flagx,(argv[i][0] == '-' && (argv[i][1] == 'x' || (argv[i][2] != '\0' && argv[i][2] == 'x')) ? 1 : 0));
         if(argv[i][0] == '-' && (argv[i][1] == 'n' || argv[i][1] == 'x' ))
             continue;
-        printf("Not flags\n");
+        // printf("Not flags\n");
         if(flagPattern == 1) {
-                    printf("%d%dfile: %s",flagn,flagx, argv[i]);
 
             flagFiles = 1;
             FILE *fptr = fopen(argv[i],"r");
             if(fptr == NULL){
+                // printf("OUT\n");
                 flagFiles = 0;
                 break;
             }
-            printf("File: %s\n",argv[i]);
+
+            // printf("File: %s\n",argv[i]);
             //find the pattern
             char line[1000];
-            int contPatteren = 0;
             int lineNum = 1;
-            int x = 0;
-            int patternFull = 0;
             int end = 0;
-            while(1){
-                while(1) {
-                    if(fgets(line, 1000, fptr) != NULL) {
-                        end = 1;
-                        break;
-                    }
-                    for(int i = 0; i < 1000 || line[i] != '\n'; i++) {
-                        
-                        if(pattern[x] == line[i]){
-                            contPatteren = 1;
-                            x++;
-                        }
-                        else
-                        {
-                            contPatteren = 0;
-                            x = 0;
-                            patternFull = 0;
-                        }
-                        
-                        if(pattern[x] == '\0') {
-                            patternFull = 1;
-                            x = 0;
-                        }
-                    }
-                    //print the line
 
-                    if(flagn == 1) {
-                        printf("%d. ",lineNum);
-                    }
-
-                    if((flagx == 1 && patternFull == 0) || (flagx == 0 && patternFull == 1) ) {
-                        printf("%s", line); 
-                    }
-                                    
-                    lineNum++;
-                }
-                if(end == 1)
+            while(1) {
+                int x = 0;
+                int patternFull = 0;
+                int contPatteren = 0;
+                if(fgets(line, 1000, fptr) == NULL) {
+                    end = 1;
                     break;
-                
+                }
+                for(int i = 0; i < 1000 && line[i] != '\n'; i++) {
+                    
+                    if(pattern[x] == line[i]){
+                        contPatteren = 1;
+                        x++;
+                    }
+                    else
+                    {
+                        contPatteren = 0;
+                        x = 0;
+                        patternFull = fmax(patternFull,0);
+                    }
+                    
+                    if(pattern[x] == '\0') {
+                        patternFull = 1;
+                        x = 0;
+                    }
+                }
+                //print the line
+
+                if(flagn == 1 && ((flagx == 1 && patternFull == 0) || (flagx == 0 && patternFull == 1) )) {
+                    printf("%d. ",lineNum, line);
+                }
+
+                if((flagx == 1 && patternFull == 0) || (flagx == 0 && patternFull == 1) ) {
+                    printf("%s", line); 
+                }
+                                
+                lineNum++;
             }
+
+            // printf("Finish");
+
             
+            fclose(fptr);
         }
         else{
             pattern = argv[i];
+            // printf("find pattern\n");
             flagPattern = 1;
         }
     }
