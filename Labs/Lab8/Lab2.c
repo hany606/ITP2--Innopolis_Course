231a7c00728d8d0fd6d7cf2905d3f6b3a9fb4c57
Hany Hamed Aly Elanwar <h.hamed.elanwar@gmail.com>
2019-03-14 12:02:16 +0300

    Modify lab8-2

--- Labs/Lab8/Lab2.c
+++ Labs/Lab8/Lab2.c
@@ -2,6 +2,10 @@
 #include<stdio.h>
 #include<string.h>
 
+#define BUFFERSIZE 15
+
+
+//Singly Linked List
 struct Cell
 {
     char* str;
@@ -11,13 +15,18 @@ struct Cell
 
 typedef struct Cell* List;
 
+
+
+//Using malloc ?? to create a cell in the heap and just return the pointer to it 
 //• Write a function that takes as an argument a string and returns a cell that contains this string.
 struct Cell stringToCell(char *s) {
     struct Cell c;
     c.str = s;
-    c.nbr = 0;
+    c.nbr = 1;
     return c;
 }
+
+
 
 //• Write a function that takes as arguments a cell and a list and adds a new cell at the head of the list.
 void addFirst(struct Cell *c, List l) {
@@ -40,14 +49,42 @@ List findString(List l, char *s) {
     return NULL;
 }
 
+void traverse(List l) {
+    List ptr = l;
+    struct Cell tmp = *l;
+    //Check the first one, Check the last one, if the list is empty???
+    while(tmp.next != NULL) {
+        printf("%s", tmp.str);
+        tmp = *(tmp.next);
+
+    }
+    
+}
+
 int main() {
+
+    //Root node
+    List root;
+    struct Cell c = stringToCell("Root");
+    printf("%s\n",c.str);
+    addFirst(&c, root);
+    // printf("%s",c.str);
+    //Traverse the List
+    printf("\nTraverse the List\n");
+    traverse(root);
+    printf("\n-----------\n");
+
     // • Write a program that reads the standard input and build the
     // list of the different lines with, for each cell, the number of
     // occurrences of the line; then the program displays out each
     // line of the number of its occurrences.
-
-
-
+    // char in[BUFFERSIZE];
+    // while(fgets(in,BUFFERSIZE,stdin) != NULL) {
+    //     printf("%s",in);
+    // }
+    // //EOF == Ctrl+z
+    // printf("\nEND of File\n");
+    
     // • Modify the previous the program in such a way that it can
     // accept the names of several files as arguments; if there is no
     // name of file, then the program reads the standard input.
