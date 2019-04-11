0e80d168ead8729c9aacd64c7873e16e3a003bb6
hany606 <h.hamed.elanwar@gmail.com>
2019-04-11 12:05:48 +0300

    Lab12-2

--- Labs/Lab12/lab1.c
+++ Labs/Lab12/lab1.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include<math.h>
 
-int Sub_Key_Get(int v, const int k){
+short int Sub_Key_Get(int v, char k){
     int offset = 2;
     for(int i = 0; i < k; i++)
         offset *= 2;
@@ -72,11 +72,58 @@ short int SBox(short int v){
     }    
 }
 
+short int permute(short int v){
+    char look_up_table[] = {0,1,5,9,13,2,6,10,14,3,7,11,15,4,8,12,16};
+    //from 2 as the 1st one doesn't change
+    for(int i = 2; i <= 16; i++){
+        short int flag = v&(1<<(i-1));
+        flag >>= (i-1);
+        // printf("FLAG = %d\n",flag);
+        //Set the other bit
+        if(flag == 1)
+            v |= (1<<(look_up_table[i]-1));
+        
+        //erase the other bit
+        else
+            v &= !(1<<(look_up_table[i]-1));
+    }
+}
+
+short int encrypt(int key, short int text){
+    short int subkey;
+    for(int i = 0; i < 4; i++){
+        subkey = Sub_Key_Get(key,i);
+        subkey ^= text;
+        subkey = SBox(subkey);
+        subkey = permute(subkey);
+    }
+    subkey = permute(subkey);
+    subkey ^= Sub_Key_Get(key,4);
+    return subkey;
+}
+
+short int decrypt(int key, short int text){
+    short int subkey = text;
+    subkey ^= Sub_Key_Get(key,4);
+    subkey = permute(subkey);
+    for(int i = 0; i < 4; i++){
+        subkey = permute(subkey);
+        subkey = SBox(subkey);
+        subkey = Sub_Key_Get(key,i);
+        subkey ^= text;
+    }
+    return subkey;
+}
 
 int main(){
     // printf("%d",sizeof(short int));//2 Byte = 16 bit
     int val = 0b00111010100101001101011000111111;
     printBinary(Sub_Key_Get(val,1));
     //problem in printing
+    //Encryption
+    int k = 0b00111010100101001101011000111111;
+    short int message = 0b0010011010110111; 
+    short int encryptedMessage = 0b1011110011010110;
+    printf("Encrypted Message -> Real: %d , Our: %d\n",encryptedMessage,encrypt(k,message));
     return 0;
 }
